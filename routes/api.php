cb0ae9f398a6439fc3fbd21852600556e9c9f361
alphakoushal <koushalsethi@gmail.com>
2025-11-03 15:19:03 +0530

    branch one

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\Api\SmartInvoiceController;
 Route::get('/test', function () {
     return response()->json(['message' => 'API is working']);
 });
+Route::get('/fetch-data', function () {
+    return response()->json(['message' => 'fetch data API is working in branch one']);
+});
 Route::post('/ask-invoice', [SmartInvoiceController::class, 'ask']);
 Route::prefix('subscription')->middleware(['auth-jwt'])->group(function () {
      return response()->json(['message' => 'Auth API is working']);

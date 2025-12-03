353e12770835d5e9e4d67e1a255d6ad18056b799
alphakoushal <koushalsethi@gmail.com>
2025-12-03 22:39:01 +0530

    merging chanings news

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/test', function () {
-    return response()->json(['message' => 'API is working']); //tets
+    return response()->json(['message' => 'API is working']); //tets new
 });
